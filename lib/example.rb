8cfefd67e9bf39a1ae6e8a223c7e4a495532c705
Kevin <kevinarogers1@gmail.com>
2021-02-02 08:53:38 -0500

    commit tree fdd81bb575e5feac78b929edb2f0cf02a9081f73

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,3 @@
-# Use this space to code along with the readme
-
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -10,4 +8,4 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-puts phrase
+puts phrase 
